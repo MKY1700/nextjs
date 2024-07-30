6375ce641aaed07549c0c2eff8d1f440e933c890
MKY1700 <a64051700@gmail.com>
2024-07-30 15:40:38 +0900

    7_30

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,8 +1,10 @@
 export default function Write() {
     return (
-        <div>
+        <div className="p-20">
             <h4>글 작성</h4>
-            <form action="/api/test" method="POST">
+            <form action="/api/post/new" method="POST">
+                <input name="title" placeholder="글 제목"></input>
+                <input name="connent" placeholder="글 내용"></input>
                 <button type="submit">제출</button>
             </form>
         </div>
--- forum/pages/api/list.js
+++ forum/pages/api/list.js
@@ -1,5 +1,9 @@
-export default function handler(요청,응답) {
-    if (요청.method == 'GET') {
-        응답.status(200).json({name : 'GET입니다'});
-    }
+import { ObjectId } from "mongodb";
+import {connectDB} from "@/utils/database.js";
+
+export default async function list(요청,응답) {
+    let db = (await connectDB).db('forum');
+    let result = await db.collection('post').find().toArray();
+
+    응답.status(200).json({name : result});
 }
